a84b0d7df2328b63e2414facdcba4c4b240553ce
Simon Jefford <simon.jefford@gmail.com>
2009-09-21 21:53:00 +0100

    Refactored fixture helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,12 @@ require 'shoulda'
 
 class Test::Unit::TestCase
   def load_fixture_as_io(fixture)
-    File.open(File.join(File.dirname(__FILE__), "fixtures", fixture), "r")
+    File.open(fixture_path(fixture), "r")
+  end
+
+  private
+
+  def fixture_path(fixture)
+    File.join(File.dirname(__FILE__), "fixtures", fixture)
   end
 end
